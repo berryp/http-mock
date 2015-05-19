fd135bec5f6620593da49c1e6cfef2dff54741e0
Berry Phillips <berryphillips@gmail.com>
2015-05-19 13:58:02 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# http-mock
+
+An HTTP server for mocking responses.
+
+## Usage
+
+`./httpm <flags>`
+
+## Options
+
+### delay
+
+The delay in seconds before responding.
+
+### status
+
+The status code of the response.
---
+++ httpm.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"net/http"
+	"time"
+)
+
+var (
+	delay int
+	statusCode int
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(time.Duration(delay) * time.Second)
+
+	w.WriteHeader(statusCode)
+	io.WriteString(w, "Hello world!")
+}
+
+func main() {
+	port := flag.String("port", "8000", "The server port.")
+
+	flag.IntVar(&delay, "delay", 0, "Response delay.")
+	flag.IntVar(&statusCode, "status", 200, "Response status code.")
+
+	flag.Parse()
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+*port, nil)
+}
+
+
