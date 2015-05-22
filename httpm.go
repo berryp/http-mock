53ed597244716d36a585c2a37eb112cb85cace58
Berry Phillips <berryphillips@gmail.com>
2015-05-22 12:16:12 +0100

    Add body and status code support.

--- httpm.go
+++ httpm.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -11,20 +12,38 @@ import (
 var (
 	delay int
 	statusCode int
+	contentType string
+	fileName string
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(time.Duration(delay) * time.Second)
 
+	w.Header().Set("Content-Type", contentType)
 	w.WriteHeader(statusCode)
-	io.WriteString(w, "Hello world!")
+
+	if (len(fileName) == 0) {
+		io.WriteString(w, "")
+		return
+	}
+
+	body, err := ioutil.ReadFile(fileName)
+	if (err != nil) {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	io.WriteString(w, string(body))
 }
 
 func main() {
 	port := flag.Int("port", 8000, "The server port.")
 
 	flag.IntVar(&delay, "delay", 0, "Response delay.")
-	flag.IntVar(&statusCode, "status", 200, "Response status code.")
+	flag.IntVar(&statusCode, "status-code", 200, "Response status code.")
+	flag.StringVar(&contentType, "content-type", "text/plain",
+			"Response content type.")
+	flag.StringVar(&fileName, "filename", "", "Response body filename.")
 
 	flag.Parse()
 
